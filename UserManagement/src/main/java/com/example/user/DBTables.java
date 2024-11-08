fda3c6403337ef7437ece65689c3bff1e220bc0d
manhh0705 <manhh0705@gmail.com>
2024-11-08 08:16:41 +0700

    up user(them lich vs hover effect)

---
+++ UserManagement/src/main/java/com/example/user/DBTables.java
@@ -0,0 +1,31 @@
+package com.example.user;
+import java.sql.*;
+
+@SuppressWarnings("CallToPrintStackTrace")
+public class DBTables {
+    public static Connection DBConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "0730AyUu");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+        public static void main(String[] args) {
+            try {
+                Connection conn = DBTables.DBConnection();
+                Statement ppapStatement = conn.createStatement();
+                String str = "SELECT * FROM users";
+                ResultSet resSet = ppapStatement.executeQuery(str);
+                while (resSet.next()) {
+                    System.out.print("user: ");
+                    System.out.print(resSet.getString("username"));
+                    System.out.print(" - pass: ");
+                    System.out.print(resSet.getString("password"));
+                    System.out.println();
+                }
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+}
